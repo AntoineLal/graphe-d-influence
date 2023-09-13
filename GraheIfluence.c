300c1db59bbcb7743cb48e028ad37a9b19f1f9e9
eudesBr1 <eudesb@hotmail.com>
2023-09-13 16:01:15 +0200

    free + sommets +lmc

--- GraheIfluence.c
+++ GraheIfluence.c
@@ -1,145 +1,41 @@
 #include "Mabiblio.h"
 
-int test = 0;
-
 
 int main() {
-    srand(time(NULL));
-    int ordre;
-    char nom[32];
+    //definit l'ordre du graphe
+    int ordre = 7;
+    ///alloue memoire tableau noms
+    char **tableauNom=NULL;
+    tableauNom = (char **)malloc(ordre * sizeof(char *));
+    if (tableauNom == NULL) {
+        perror("Erreur d'allocation de mémoire");
+        return 1;
+    }
+    for (int i = 0; i < ordre; i++) {
+        tableauNom[i] = (char *)malloc(19 * sizeof(char)); // chaque nom peut contenir jusqu'à 20 caractères
+        if (tableauNom[i] == NULL) {
+            perror("Erreur d'allocation de mémoire pour le nom");
+            return 1;
+        }
+    }
+    ///remplit tableau noms
+    strcpy(tableauNom[0],"FrancoisS");
+    strcpy(tableauNom[1],"FabienneC");
+    strcpy(tableauNom[2],"JulienneP");
+    strcpy(tableauNom[3],"LouisM");
+    strcpy(tableauNom[4],"BoubakerD");
+    strcpy(tableauNom[5],"IlhemF");
+    strcpy(tableauNom[6],"JPS");
+
+    ///initalise le graphe
     t_graphe graphe;
-    printf("\nsaisissez l'odre ");
-    scanf("%d",&ordre);
-    fflush(stdin);
-    initialisationGraphe(ordre,&graphe);
-    //initGrapheRandom(ordre,&graphe);
-    printf("test %d\n",test);
-    afficherInfluence(graphe);
-    printf("\nsaisissez le nom du fichier ou enregistrer les donnees ; ");
-    gets(nom);
+    graphe = initGraphe(ordre,graphe,tableauNom);
+    printf("test");
+    for (int i = 0; i < graphe.ordre; ++i) {
+        printf("le nom du sommet %d est %s\n",graphe.sommets[i].numero,graphe.sommets[i].nom);
+    }
+    free(graphe.sommets);
+    free(graphe.matrice);
+    free(tableauNom);
     return 0;
-}
-
-t_sommet initSommet(int numero,char *nom)
-{
-    t_sommet sommetRetourner;
-    sommetRetourner.nom = malloc((strlen(nom) + 1) * sizeof(char));
-    strcpy(sommetRetourner.nom,nom);
-    sommetRetourner.nbrSommet = numero;
-    return sommetRetourner;
-}
-
-void initGrapheRandom(int ordre,t_graphe *graphe)
-{
-    graphe->ordre = ordre;
-    graphe->matriceAdjacence = malloc(sizeof(int)*ordre*ordre);
-    graphe->tabDynSom = malloc(sizeof(t_sommet)*ordre);
-
-    ///initialisation des influances de manieres aléatoires
-
-    for (int i=0;i<graphe->ordre;i++)
-    {
-        for (int b=0;b<graphe->ordre;b++)
-        {
-            graphe->matriceAdjacence[i][b] = rand()%2;
-        }
-    }
-    printf("test 1111\n");
-
-    ///initialisation de tous les sommets
-
-    for (int i = 0; i < graphe->ordre; i++)
-    {
-        //  initSommet(i,());
-    }
-}
-
-
-
-void initialisationGraphe(int ordre, t_graphe *graphe) {
-    graphe->ordre = ordre;
-    graphe->matriceAdjacence = malloc(sizeof(int) * ordre * ordre);
-    graphe->tabDynSom = malloc(sizeof(t_sommet) * ordre);
-
-    if (graphe->matriceAdjacence == NULL || graphe->tabDynSom == NULL) {
-        // Handle memory allocation failure here
-        printf("Memory allocation failed.\n");
-        return;
-    }
-
-    for (int i = 0; i < graphe->ordre; i++) {
-        printf("Saisissez le nom de la personne %d: ", i);
-        scanf("%s", graphe->tabDynSom[i].nom);
-
-        for (int b = 0; b < graphe->ordre; b++) {
-            if (i != b) {
-                printf("%s influence %s ? (1 pour oui 0 pour non): ", graphe->tabDynSom[i].nom, graphe->tabDynSom[b].nom);
-                scanf("%d", &graphe->matriceAdjacence[i][b]);
-            } else {
-                graphe->matriceAdjacence[i][b] = 0;
-            }
-        }
-        // Initialize the t_sommet structure
-        graphe->tabDynSom[i] = initSommet(i, graphe->tabDynSom[i].nom);
-    }
-}
-
-
-void testInfluence(t_graphe graphe,int i,int b){
-    if(graphe.matriceAdjacence[i][b]==1)
-    {
-        printf("\n%s influence %s",graphe.tabDynSom[i].nom,graphe.tabDynSom[b]);
-    }
-    else{
-        printf("\n%s n influence pas %s",graphe.tabDynSom[i].nom,graphe.tabDynSom[b]);
-    }
-}
-
-void afficherInfluence(t_graphe graphe){
-    for (int i=0;i<graphe.ordre;i++)
-    {
-        for (int b=0;b<graphe.ordre;b++)
-        {
-            testInfluence(graphe,i,b);
-        }
-    }
-}
-
-void enregistrerTableauDansFichier(t_graphe  graphe, const char *fichierSave) {
-    FILE *fichier = fopen(fichierSave, "w"); // Open the file in write mode
-
-
-    if (fichier == NULL) {
-        printf("Erreur lors de l'ouverture du fichier.\n");
-        return;
-    }
-
-    // Parcourir le tableau et enregistrer chaque élément dans le fichier
-    for (int i = 0; i < graphe.ordre; i++) {
-        for (int j = 0; j < graphe.ordre; j++) {
-            fprintf(fichier, "%d ", graphe.matriceAdjacence[i][j]);
-        }
-        fprintf(fichier, "\n"); // Passer à la ligne suivante dans le fichier
-    }
-
-    fclose(fichier); // Fermer le fichier
-}
-
-void recupererTableauDansFichier(t_graphe  *graphe, const char *fichierSave) {
-    FILE *fichier = fopen(fichierSave, "r"); // Ouvrir le fichier en mode écriture
-
-    if (fichier == NULL) {
-        printf("Erreur lors de l'ouverture du fichier.\n");
-        return;
-    }
-
-    // Parcourir le tableau et enregistrer chaque élément dans le fichier
-    for (int i = 0; i < graphe->ordre; i++) {
-        for (int j = 0; j < graphe->ordre; j++) {
-            fscanf(fichier, "%d", &graphe->matriceAdjacence[i][j]);
-        }
-        fprintf(fichier, "\n"); // Passer à la ligne suivante dans le fichier
-    }
-
-    fclose(fichier); // Fermer le fichier
 }
--- Mabiblio.h
+++ Mabiblio.h
@@ -14,20 +14,39 @@
 #endif //THEORIEGRAPHE_MABIBLIO_H
 
 
+//
+// Created by eudes on 13/09/2023.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define taille 2
+
 typedef struct sommet{
     char *nom;
-    int nbrSommet;
+    int numero;
 }t_sommet;
-
+t_sommet initSommet(int numero,char *nom)
+{
+    t_sommet sommet;
+    sommet.numero =(int) malloc(sizeof(int ));
+    sommet.numero = numero; /// numero du sommet
+    sommet.nom = (char*)malloc(sizeof(char) * (strlen(nom) + 1));
+    strcpy(sommet.nom,nom);///nom du sommet
+    return sommet;
+}
 typedef struct graphe{
-    int **matriceAdjacence;
-    int ordre;
-    t_sommet* tabDynSom;
+    int ordre;          /// L'ordre
+    int **matrice;      /// La matrice d'adjacence dynamique d'entiers
+    t_sommet *sommets;  /// Un tableau dynamique de sommets
 }t_graphe;
-t_sommet initSommet(int numero,char *nom);
-
-void  initialisationGraphe(int ordre,t_graphe *graphe);
-
-void initGrapheRandom(int ordre,t_graphe *graphe);
-
-void afficherInfluence(t_graphe graphe);
+t_graphe initGraphe(int ordre,t_graphe graphe,char **tableauNom)
+{
+    graphe.ordre =(int) malloc(sizeof(int ));
+    graphe.ordre = ordre;
+    graphe.sommets = malloc(sizeof (t_sommet)*ordre);
+    for (int i = 0; i < ordre; ++i) {
+        graphe.sommets[i] = initSommet(i,tableauNom[i]);
+    }
+    return graphe;
+}
