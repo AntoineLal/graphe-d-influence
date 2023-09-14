51f9be4618c98c929d82095464bb5d91c16aa8c2
AntoineLal <antoine.nl.lallement@gmail.com>
2023-09-14 10:49:49 +0200

    menu

--- GraheIfluence.c
+++ GraheIfluence.c
@@ -51,7 +51,7 @@ int main() {
     t_graphe graphe;
     int menu;
     do {
-        printf("0 pour quitter  1 pour immprimer les sommets 2 pour imprimer le graphe d influence 3 pour charger graphe 4 pour sauvegarder graphe 5 pour charge automatique\n");
+        printf("\n0 pour quitter  1 pour immprimer les sommets 2 pour imprimer le graphe d influence\n 3 pour charger graphe 4 pour sauvegarder graphe 5 pour charge automatique\n");
         scanf("%d",&menu);
         if (menu == 1){
             printf("l'odre est :%d\n",graphe.ordre);
@@ -61,6 +61,8 @@ int main() {
             }
         }
         if(menu==2){afficaheInflu(graphe);}
+        if(menu==3){nomFichiercharge(&graphe);}
+        if(menu==4){ nomFichierSave(graphe);}
         if(menu==5){graphe = charge("../test.txt",graphe);}
     } while (menu !=0);
 
